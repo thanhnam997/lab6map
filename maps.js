1c8c5535ff912c2f2a75bf596b82995fc7d5cbab
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-02-24 00:37:25 -0600

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,4 @@
+ let  metroAreaCenterCoordinates =[44.96,-93.2]
+ let zoomLevel = 9 // 1=  whole world 20 = city blocks
+
+ let map = l.map ('college-map'). setView(metroAreaCenterCoordinates, zoomLevel)
